cccc628aa450d930f70bf38fbe152fb9276bc4b1
kentaueno <kentaueno1995@icloud.com>
2025-09-22 23:59:16 +0900

    Since taskForm was created, the method for accessing properties has changed.

--- resources/views/livewire/create-task-modal.blade.php
+++ resources/views/livewire/create-task-modal.blade.php
@@ -5,8 +5,8 @@
                 <flux:heading size="lg">Create task</flux:heading>
                 <flux:text class="mt-2">Let's create your task.</flux:text>
             </div>
-            <flux:input wire:model="title" label="Title" placeholder="Enter Title" />
-            <flux:textarea wire:model="description" label="Description" placeholder="Enter description." />
+            <flux:input wire:model="form.title" label="Title" placeholder="Enter Title" />
+            <flux:textarea wire:model="form.description" label="Description" placeholder="Enter description." />
             <div class="flex">
                 <flux:spacer />
                 <flux:button type="submit" variant="primary">Create</flux:button>
