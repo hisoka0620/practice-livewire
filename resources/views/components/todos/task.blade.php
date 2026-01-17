22f00b70a5655c417005dc08048197c0781af9a6
kentaueno <kentaueno1995@icloud.com>
2026-01-17 14:54:51 +0900

    Display badges on tasks that are due soon or have passed their due date

--- resources/views/components/todos/task.blade.php
+++ resources/views/components/todos/task.blade.php
@@ -1,13 +1,19 @@
 <div>
     <div @class([
         'mb-3 rounded bg-white p-4 border-l-4',
-        'border-red-500 bg-red-100!' =>
-            $task->deadline_status === 'overdue',
-        'border-yellow-500 bg-yellow-100' =>
-            $task->deadline_status === 'due_soon',
+        'border-red-500 bg-red-100!' => $task->deadline_status === 'overdue',
+        'border-yellow-500 bg-yellow-100' => $task->deadline_status === 'due_soon',
     ])>
-        <div class="flex flex-col">
+        <div class="flex flex-col gap-y-6">
             <div>
+                @if ($task->deadline_status === 'overdue')
+                    <flux:badge variant="solid" color="red" class="mb-2" icon="exclamation-triangle">
+                        Overdue</flux:badge>
+                @elseif($task->deadline_status === 'due_soon')
+                    <flux:badge variant="solid" color="yellow" class="text-white! mb-2" icon="exclamation-circle">
+                        Due Soon
+                    </flux:badge>
+                @endif
                 <div class="grid grid-cols-1 gap-4 md:grid-cols-5">
                     <x-todos.task-item heading="Title">
                         {!! $this->highlight($task->title) !!}
